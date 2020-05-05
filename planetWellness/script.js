0ba39ec6598f71f5b2e7ca3c73942528dc0ea9d4
Mayara Caldeira <mayaracsmith@gmail.com>
2020-05-05 16:44:26 -0400

    index + api

--- planetWellness/script.js
+++ planetWellness/script.js
@@ -4,3 +4,31 @@ const navBar = document.querySelector(".navbar")
 hamburgerIcon.addEventListener('click', () => {
     navBar.classList.toggle("change")
 })
+
+function validateForm() {
+    var name = document.forms["form"]["name"].value;
+    var lastName = document.forms["form"]["lastname"].value;
+    var email = document.forms["form"]["email"].value
+    if (name == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if (lastName == "") {
+        alert("Last name must be filled out");
+        return false;
+    }
+    if(email == ""){
+        alert("Email must be filled out");
+        return false;
+    }
+}
+
+
+const AIR = document.getElementById("air") = 
+`<% if(air !== null){ %>
+    <p><%= air %></p>
+  <% } %>
+
+  <% if(error !== null){ %>
+    <p><%= error %></p>
+  <% } %>`
